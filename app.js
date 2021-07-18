38c0fa95aa5f1192b84bbf449610f6be7f57ca47
welinton developer <welintondeveloper@gmail.com>
2021-07-18 17:22:15 -0300

    Converter Fahrenheit Para Graus Celsius

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log( 'Ola Mundo' );//o ponto e virgula no final em java script e opcional
+// console.log( 'Ola Mundo' );//o ponto e virgula no final em java script e opcional
+
+//convertter Fahrenheit para graus Celsius  Fórmula	(90 °F − 32) × 5/9 = 32,222 °C
+
+
+let f = 90;//fahrenheit
+let c; //celsius
+
+c = (f - 32) * 5/9;//números entre parenteses terá prioridade na ordem de cálculo
+
+console.log(c);
