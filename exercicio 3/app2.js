45722e9a79588b02706c8a79e9101ab7df6966f2
welinton developer <welintondeveloper@gmail.com>
2021-07-19 22:58:42 -0300

    Exemplos De Estruturas De Controle If Else

---
+++ exercicio 3/app2.js
@@ -0,0 +1,14 @@
+
+
+let nota = 60;
+console.log('nota '+nota);
+if (nota > 59) {
+  console.log('Você foi aprovado');
+}else{
+  if(nota < 50){
+    console.log('Porfavor Estude mais')
+  }else{
+    console.log('Você foi Reprovado');
+  }
+}
+console.log('Boas Férias');
