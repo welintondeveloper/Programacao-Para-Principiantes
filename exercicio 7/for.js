7bce40909789c85d8d643480054ee5c92f729aca
welinton developer <welintondeveloper@gmail.com>
2021-07-21 00:05:45 -0300

    Projeto Tabuada * Com Qualquer Número

---
+++ exercicio 7/for.js
@@ -0,0 +1,10 @@
+console.log('exercício 6 Tabuada for');
+
+
+let calcularNumero = 9;//aqui vai o numero que pretendo calcular
+let resultado = 0;
+for(i = 0; i<=10;i++){
+  console.log(calcularNumero + '  X  '+i+' '+'=  '+ resultado);//concatenação apenas para exibir os resultados
+  resultado += calcularNumero;
+}
+
