246be7cbcaf19faed9e23963a1cca396cee1928e
welinton developer <welintondeveloper@gmail.com>
2021-07-21 22:41:44 -0300

    Exemplo Básico Sobre Arrays

---
+++ exercicio 11/arraysBasicos.js
@@ -0,0 +1,6 @@
+console.log('exercício 11 arrays Básicos');
+
+
+let flores = ['rosa',"lirio",'gardenia','girasol'];//é um padrão recomendado usar aspas simples ao invés de duplas 
+
+console.log(flores[0]);//o arrays começa a contar da posiçao 0 e não 1
