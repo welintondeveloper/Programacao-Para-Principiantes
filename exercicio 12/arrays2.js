4166e0bc513bf769c2de7ec690c6bbcaa937d85a
welinton developer <welintondeveloper@gmail.com>
2021-07-22 00:27:15 -0300

    Exemplo Dois Varrer Dois Arrays Com For

---
+++ exercicio 12/arrays2.js
@@ -0,0 +1,12 @@
+console.log('exercício 12 arrays Exemplo 2');
+
+let salario = [1086, 2215, 1580]
+let pessoas = ['Welinton', 'Gustavo', 'Rebeca'];
+
+//cada volta o [i] terá o valor do array pessoas acrecentado +1
+for (i = 0; i < salario.length; i++) {
+
+  //da mesma forma acontece com o array salário
+  console.log('\n'+pessoas[i] + ' Tem o salário de: ' + salario[i]);
+}
+
