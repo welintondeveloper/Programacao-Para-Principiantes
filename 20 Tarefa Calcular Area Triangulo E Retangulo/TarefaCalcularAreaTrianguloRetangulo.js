0e9f82ce2d95d03b14244f5e77ee18cdf32bad52
welinton developer <welintondeveloper@gmail.com>
2021-07-25 20:12:50 -0300

    Tarefa Calcular Área De Um Triangulo Retangulo

---
+++ 20 Tarefa Calcular Area Triangulo E Retangulo/TarefaCalcularAreaTrianguloRetangulo.js
@@ -0,0 +1,13 @@
+console.log('Tarefa Calcular Área Triângulo Retângulo');
+
+//formula para calcular a area de um triangulo retangulo e base multiplicado por altura dividido por dois
+
+//base 10
+//altura 5
+function areaTriRetangulo(base, altura){
+
+  return area = (base * altura) / 2 ;//não é nescesario colocar entre () mais fica mais fácil a leitura assim
+}
+
+
+console.log(areaTriRetangulo(10, 5));
